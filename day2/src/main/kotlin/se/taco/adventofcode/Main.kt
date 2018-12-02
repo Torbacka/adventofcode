27c596fc06293e002450d8aaef3b92f33eb4d066
Daniel Törbacka <daniel.torbacka@gmail.com>
2018-12-02 16:29:42 +0100

    Solved day2 problem 2

--- day2/src/main/kotlin/se/taco/adventofcode/Main.kt
+++ day2/src/main/kotlin/se/taco/adventofcode/Main.kt
@@ -5,6 +5,24 @@ import java.nio.file.Paths
 
 fun main(args: Array<String>) {
     val data: List<String> = Files.readAllLines(Paths.get("src/main/resources/data.txt"))
-    
+    var words: MutableSet<String> = mutableSetOf()
+    for (line in data) {
+        val newWords = getWords(line)
+        for (word in newWords) {
+            if (words.contains(word)) {
+                println(word)
+            } else {
+                words.add(word)
+            }
+        }
+    }
+}
+
+fun getWords(word: String): Set<String> {
+    var words: MutableSet<String> = mutableSetOf()
+    for (i in (0..(word.length - 1))) {
+        words.add(word.substring(0,i) + word.subSequence(i+1,word.length))
+    }
+    return words
 }
 
