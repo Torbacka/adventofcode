3fed1278e63172eb01dc1a9821fef50253d4e9f9
Daniel Törbacka <daniel.torbacka@gmail.com>
2018-12-02 16:32:43 +0100

    Prepear day3

---
+++ day3/src/main/kotlin/se/taco/adventofcode/Main.kt
@@ -0,0 +1,10 @@
+package se.taco.adventofcode
+
+import java.nio.file.Files
+import java.nio.file.Paths
+
+fun main(args: Array<String>) {
+    val data: List<String> = Files.readAllLines(Paths.get("src/main/resources/data.txt"))
+    
+}
+
