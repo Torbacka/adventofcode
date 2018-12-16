f5d5bc7d9e0d1786ec3cce8350752ca9f0e5cfc5
Daniel Törbacka <daniel.torbacka@gmail.com>
2018-12-16 19:22:09 +0100

    Trying to solve day 7 problem 2

--- day7/src/main/kotlin/se/taco/adventofcode/Main.kt
+++ day7/src/main/kotlin/se/taco/adventofcode/Main.kt
@@ -3,23 +3,49 @@ package se.taco.adventofcode
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.util.*
-import kotlin.concurrent.timer
 
 fun main(args: Array<String>) {
     val data: List<String> = Files.readAllLines(Paths.get("src/main/resources/data.txt"))
     val graph = parseData(data)
     println(graph)
-    var noIncomingEdge: PriorityQueue<String> = PriorityQueue()
-    graph.incomingEdges
+    var noIncomingEdge: PriorityQueue<String> = graph.findIncomingEdgesZero()
+    var time = 0
+    var numberOfWorkers = 2
+    var tasks: MutableList<String> = mutableListOf()
+    while (noIncomingEdge.isNotEmpty() or tasks.isNotEmpty()) {
+
+        val node: String = noIncomingEdge.poll()
+        graph.incomingEdges[node]
+        if (noIncomingEdge.isNotEmpty() && tasks.size < numberOfWorkers) {
+
+        } else {
+
+        }
+    }
+}
+
+fun <T> Graph<T>.removeEdges(node: T, noIncomingEdge: PriorityQueue<T>): PriorityQueue<T> {
+    val iterator = this.outgoingEdges[node]!!.iterator()
+    while (iterator.hasNext()) {
+        val edge = iterator.next()
+        iterator.remove()
+        this.removeEdge(edge)
+        if (this.incomingEdges[edge.fromNode]!!.isEmpty()) {
+            noIncomingEdge.add(edge.fromNode)
+        }
+    }
+    return noIncomingEdge
+}
+
+fun <T> Graph<T>.findIncomingEdgesZero(): PriorityQueue<T> {
+    var noIncomingEdge: PriorityQueue<T> = PriorityQueue()
+    this.incomingEdges
         .filter { it.value.size == 0 }
         .forEach {
             noIncomingEdge.add(it.key)
         }
-    var time = 0
-    while (noIncomingEdge.isNotEmpty()) {
-        val node: String = noIncomingEdge.poll()
-        time
-    }
+
+    return noIncomingEdge
 }
 
 fun parseData(data: List<String>): Graph<String> {
