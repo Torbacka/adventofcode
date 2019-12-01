e4a55881b17d2f8833f50044430cab9a550c35f2
Daniel Törbacka <daniel.torbacka@gmail.com>
2019-12-01 14:04:07 +0100

    Day 1 part 2

--- 2019/day1/main.py
+++ 2019/day1/main.py
@@ -4,10 +4,13 @@ pairs = [int(line.strip()) for line in open("input/input.txt").readlines()]
 
 
 def main():
-    fuel = 0
+    fuel_total = 0
     for pair in pairs:
-        fuel += math.floor(pair/3) - 2
-    print(fuel)
+        fuel = math.floor(pair/3) - 2
+        while fuel > 0:
+            fuel_total += fuel
+            fuel = math.floor(fuel / 3) - 2
+    print(fuel_total)
 
 
 if __name__ == '__main__':
