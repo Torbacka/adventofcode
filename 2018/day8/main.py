43e2ab83d753c530a43266c814c6c73352e0ebe5
Daniel Törbacka <daniel.torbacka@gmail.com>
2019-12-01 18:02:59 +0100

    WIP: trying to solve 2018 day8 part 2

--- 2018/day8/main.py
+++ 2018/day8/main.py
@@ -2,26 +2,53 @@ import re
 
 parser = re.compile("-?\d+")
 data = [int(x) for line in open("input/input.txt").readlines() for x in parser.findall(line.strip())]
-
-tree = dict()
+node_name = 'A'
 
 
 def main():
-    metadata = parse_tree(data.pop(0), data.pop(0), list(), data)
-    print(sum(metadata))
+    tree = parse_tree(data.pop(0), data.pop(0), dict(), data)
 
 
-def parse_tree(number_of_children, number_of_metadata, metadata, data):
+    print(sum_tree(tree, 0))
+
+
+def sum_tree(root, total_sum):
+    tree = next(iter(root.values()))
+    if len(tree['children']) == 0:
+        return sum(tree['metadata'])
+    else:
+        for data in tree['metadata']:
+            if len(tree['children']) < data:
+                return 0
+            else:
+                total_sum += sum_tree(tree['children'][data - 1], total_sum)
+    return total_sum
+
+
+def parse_tree(number_of_children, number_of_metadata, tree, data):
+    global node_name
+    tree = dict()
+    current_node = node_name
     if number_of_children == 0:
-        return sum(data[:number_of_metadata]), data[number_of_metadata:]
+        tree[node_name] = {
+            'metadata': data[:number_of_metadata],
+            'children': []
+        }
+        return tree, data[number_of_metadata:]
     for child in range(number_of_children):
-        pair = parse_tree(data.pop(0), data.pop(0), metadata, data)
-        metadata.extend(pair[0])
+        if current_node not in tree.keys():
+            tree[current_node] = {
+                'metadata': [],
+                'children': []
+            }
+        node_name = chr(ord(node_name) + 1)
+        pair = parse_tree(data.pop(0), data.pop(0), tree, data)
+        tree[current_node]['children'].append(pair[0])
         data = pair[1]
+    tree[current_node]['metadata'] = data[:number_of_metadata]
     if len(data[number_of_metadata:]) == 0:
-        metadata.extend(data[:number_of_metadata])
-        return metadata
-    return data[:number_of_metadata], data[number_of_metadata:]
+        return tree
+    return tree, data[number_of_metadata:]
 
 
 if __name__ == '__main__':
