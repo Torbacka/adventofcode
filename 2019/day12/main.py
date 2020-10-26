fd613823e83b023062c7bffeb5f49073a4546824
Daniel Törbacka <daniel.torbacka@gmail.com>
2020-10-26 11:15:31 +0100

    Solve day12 advent of code

--- 2019/day12/main.py
+++ 2019/day12/main.py
@@ -2,26 +2,42 @@ import re
 from sympy import Point
 
 parser = re.compile("-?\d+")
-data = {i: {"pos": list(map(int, parser.findall(line.strip()))), 'velocity': [0, 0, 0]} for i, line in enumerate(open("input/input.in").readlines())}
+data = {i: {"pos": list(map(int, parser.findall(line.strip()))), 'velocity': [0, 0, 0]} for i, line in
+        enumerate(open("input/input.in").readlines())}
 
 
 def main():
     print(data)
-    simulate()
-    print(data)
+    for i in range(1000):
+        simulate()
+    print(calculate_total_energy())
+
+
+def calculate_total_energy():
+    total_energy = 0
+    for moon in data.values():
+        potential_energy = 0
+        kinetic_energy = 0
+        for i, pos_cord in enumerate(moon['pos']):
+            potential_energy += abs(pos_cord)
+            kinetic_energy += abs(moon['velocity'][i])
+        total_energy += potential_energy * kinetic_energy
+    return total_energy
 
 
 def simulate():
-    for moon in data.values():
-        acceleration = [0, 0, 0]
-        for compare_moon in data.values():
+    for m, moon in enumerate(data.values()):
+        for n, compare_moon in enumerate(data.values()):
+            if m >= n:
+                continue
             for i, coord in enumerate(compare_moon['pos']):
                 if moon['pos'][i] < coord:
-                    acceleration[i] += 1
+                    moon['velocity'][i] += 1
+                    compare_moon['velocity'][i] -= 1
                 if moon['pos'][i] > coord:
-                    acceleration[i] -= 1
+                    moon['velocity'][i] -= 1
+                    compare_moon['velocity'][i] += 1
         for i, coord in enumerate(moon['velocity']):
-            moon['velocity'][i] += acceleration[i]
             moon['pos'][i] += moon['velocity'][i]
 
 
