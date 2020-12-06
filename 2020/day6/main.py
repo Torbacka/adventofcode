2d3cbd2a5eae58b50f38c959f4b02777f35c7094
Daniel Törbacka <daniel.torbacka@gmail.com>
2020-12-06 06:21:52 +0100

    Day 6

--- 2020/day6/main.py
+++ 2020/day6/main.py
@@ -1,11 +1,22 @@
 import re
 
-parser = re.compile("-?\d+")
-data = [int(x) for line in open("input/input.in").readlines() for x in parser.findall(line.strip())]
+group = set()
+count = 0
+new_group = True
+for line in open("input/input.in").readlines():
+    if len(line) == 1:
+        count += len(group)
+        group = set()
+        new_group = True
+    else:
+        if new_group:
+            group = line.strip()[:]
+            new_group = False
+        else:
+            temp = set()
+            for c in line.strip():
+                if c in group:
+                    temp.add(c)
+            group = temp
 
-def main():
-    print(pairs)
-
-
-if __name__ == '__main__':
-    main()
+print(count)
