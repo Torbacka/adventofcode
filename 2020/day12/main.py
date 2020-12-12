e9072e87aea56be0468775dc167d3f1471a5e2ae
Daniel Törbacka <daniel.torbacka@gmail.com>
2020-12-12 07:37:49 +0100

    AOC day 12

--- 2020/day12/main.py
+++ 2020/day12/main.py
@@ -1,11 +1,44 @@
-import re
+from math import cos, sin, radians
 
-parser = re.compile("-?\d+")
-data = [int(x) for line in open("input/input.in").readlines() for x in parser.findall(line.strip())]
+east = 0
+north = 0
+heading = 1
+headings = ['N', 'E', 'S', 'W']
+waypoint = [10, 1]
 
-def main():
-    print(pairs)
+
+def rotate(d):
+    global heading
+    radian = radians(d)
+    new_x = round(waypoint[0] * cos(radian) + waypoint[1] * sin(radian))
+    new_y = round(-waypoint[0] * sin(radian) + waypoint[1] * cos(radian))
+    waypoint[0] = new_x
+    waypoint[1] = new_y
 
 
-if __name__ == '__main__':
-    main()
+def update_distance(value, direction):
+    global north, east
+    if direction == 'N':
+        waypoint[1] += value
+    elif direction == 'S':
+        waypoint[1] -= value
+    elif direction == 'E':
+        waypoint[0] += value
+    elif direction == 'W':
+        waypoint[0] -= value
+
+
+for line in open("input/input.in"):
+    action = line[:1]
+    value = int(line[1:])
+    if action == 'L':
+        rotate(360 - value)
+    elif action == 'R':
+        rotate(value)
+    elif action == 'F':
+        north += value * waypoint[1]
+        east += value * waypoint[0]
+    else:
+        update_distance(value, action)
+
+print(abs(east) + abs(north))
