802ddae039fe917e0d8327da65d84f0de0076ce8
Daniel Törbacka <daniel.torbacka@gmail.com>
2021-12-05 10:41:20 +0100

    Even better solution

--- 2021/day5/main.py
+++ 2021/day5/main.py
@@ -1,22 +1,15 @@
-data = [line.strip() for line in open("input/input.in").readlines()]
+from collections import defaultdict
+import re
+data = [list(map(int, re.findall("-?\d+", line))) for line in open("input/input.in").readlines()]
 
 
 def main():
-    grid = {}
-
-    for line in data:
-        start, end = line.split(" -> ")
-        points = points_on_line(convert_to_int(start.split(",")), convert_to_int(end.split(",")))
+    matrix = defaultdict(lambda: 0)
+    for x1, y1, x2, y2 in data:
+        points = points_on_line(convert_to_int((x1, y1)), convert_to_int((x2, y2)))
         for point in points:
-            if point not in grid:
-                grid[point] = 1
-            else:
-                grid[point] += 1
-    sum = 0
-    for point, hits in grid.items():
-        if hits > 1:
-            sum += 1
-    print(sum)
+                matrix[point] += 1
+    print(sum(1 for v in matrix.values() if v >= 2))
 
 
 def convert_to_int(string_numbers):
@@ -24,30 +17,20 @@ def convert_to_int(string_numbers):
 
 
 def points_on_line(point1, point2):
-    points = []
-    # Check vertical line
+    yield point2
+    # Vertical line
     if point1[0] == point2[0]:
         for y in range(point1[1], point2[1], 1 if point1[1] < point2[1] else -1):
-            points.append((point1[0], y))
-
-    # check horizontal line
+            yield point1[0], y
+    # Horizontal line
     elif point1[1] == point2[1]:
         for x in range(point1[0], point2[0], 1 if point1[0] < point2[0] else -1):
-            points.append((x, point1[1]))
-
+            yield x, point1[1]
     else:
         k = int((point1[1] - point2[1]) / (point1[0] - point2[0]))
-        m = point1[1] - (k * point1[0])
-        step = k
-        if k < 0 and point1[0] < point2[0]:
-            step *= -1
-        elif k > 0 and point1[0] > point2[0]:
-            step *= -1
-        for x in range(point1[0], point2[0], step):
-            y = k * x + m
-            points.append((x, y))
-    points.append(point2)
-    return points
+        step = 1 if point1[0] < point2[0] else -1
+        for m in range(0, (point2[0] - point1[0]), step):
+            yield (point1[0] + m), (point1[1] + m * k)
 
 
 if __name__ == '__main__':
