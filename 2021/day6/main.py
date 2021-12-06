6cb9ba743ba55fb745704c4153ac3db674af8e66
Daniel Törbacka <daniel.torbacka@gmail.com>
2021-12-06 06:41:56 +0100

    2021 day6

--- 2021/day6/main.py
+++ 2021/day6/main.py
@@ -1,11 +1,26 @@
 import re
+from collections import defaultdict
 
-parser = re.compile("-?\d+")
-data = [int(x) for line in open("input/input.in").readlines() for x in parser.findall(line.strip())]
+data = [list(map(int, re.findall("-?\d+", line))) for line in open("input/input.in").readlines()]
+
 
 def main():
-    print(pairs)
+    fishs = defaultdict(lambda: 0)
+    for fish in data[0]:
+        fishs[fish] += 1
 
+    for i in range(0,256 ):
+        temp_fishs = defaultdict(lambda: 0)
+        for fish, number in fishs.items():
+            fish -= 1
+            if fish == -1:
+                temp_fishs[8] += number
+                fish = 6
+                temp_fishs[fish] += number
+            else:
+                temp_fishs[fish] += number
+        fishs = temp_fishs
+    print(sum(fishs.values()))
 
 if __name__ == '__main__':
     main()
