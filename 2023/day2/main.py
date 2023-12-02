fbd91cf37e32b081f83b3e85d86c9e5f53d8b746
Daniel Törbacka <daniel.torbacka@gmail.com>
2023-12-02 06:38:25 +0100

    Solved day2 2023

---
+++ 2023/day2/main.py
@@ -0,0 +1,26 @@
+import re
+
+parser = re.compile(r"-?\d+")
+data = [line.strip() for line in open("input/input.in").readlines()]
+
+
+sum = 0
+for line in data:
+    bags = {
+        "red": 1,
+        "green": 1,
+        "blue": 1
+    }
+    game = line.split(":")
+    id = int(parser.findall(game[0])[0])
+    for set in game[1].strip().split(";"):
+        for color in set.strip().split(","):
+            value, color = color.strip().split(" ")
+            if bags[color] < int(value):
+                bags[color] = int(value)
+
+
+
+    sum += bags["red"]*bags["green"]*bags["blue"]
+
+print(sum)
